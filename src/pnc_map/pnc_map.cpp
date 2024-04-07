71264c6bc87da599b5aa829b715ee3caf00b1bf9
luxiangj <sy2307314@buaa.edu.cn>
2024-04-07 21:08:56 +0800

    basic mode

---
+++ src/pnc_map/pnc_map.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "pnc_map.h"
+using namespace std;
+void PNC_Map::map_info()
+{
+    cout<<"this is pnc_map"<<endl;
+}
---
+++ src/process/process.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "process.h"
+using namespace std;
+
+void Process::planProcess()
+{
+    cout<<"this is planning process"<<endl;
+    my_map.map_info();
+    cout<<"planning success"<<endl;
+
+
+} 
